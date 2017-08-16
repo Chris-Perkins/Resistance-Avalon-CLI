781e3cc1d6ac9fac25ecf9bd06a46eb5e30a14f5
neptunicjenkins <chrisfromtemporaryid@gmail.com>
2017-08-15 21:28:13 -0400

    Whoops. Input text goes in input, not split.

--- avalon.py
+++ avalon.py
@@ -74,7 +74,7 @@ def main():
 
 # get the names of every player
 def get_list_players():
-    return [x.lower() for x in input().split("Enter the names of the players separated by a space\n> ")]    
+    return [x.lower() for x in input("Enter the names of the players separated by a space\n> ").split()]    
 
 # let the player chose what roles will be activated throughout the game
 def chose_possible_roles(player_count):
